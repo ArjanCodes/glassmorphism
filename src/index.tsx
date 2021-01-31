362ee7fc38f590d1774b53f9a01589c2ec347732
Arjan <arjan@quarterfall.com>
2021-01-31 14:50:05 +0100

    Added eslintcache to gitignore.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./AppBefore";
+import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
